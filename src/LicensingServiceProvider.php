328402dccd65ee640bf81d6b7125a681308ba04d
mdazizi000 <69894670+mdazizi000@users.noreply.github.com>
2023-05-09 11:46:29 +0430

    debuge

--- src/LicensingServiceProvider.php
+++ src/LicensingServiceProvider.php
@@ -1,8 +1,23 @@
 <?php
 namespace Packages\Licensing;
 use Illuminate\Support\ServiceProvider;
+use Packages\Licensing\Classes\License;
 
 class LicensingServiceProvider extends ServiceProvider
 {
+    public function boot()
+    {
+//        $this->publishes(
+//            [
+//                __DIR__.'/../config/vandar.php'=>config_path('vandar.php')
+//            ]
+//        );
+    }
 
+    public function register()
+    {
+        $this->app->singleton(License::class,function (){
+            return new License();
+        });
+    }
 }
